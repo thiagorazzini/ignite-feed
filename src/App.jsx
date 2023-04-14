00c5c1f9616446f06889ddcabc913be32c647671
thiagorazzini <razzini.thiago@gmail.com>
2023-04-14 17:03:02 -0300

    Done the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+import "./global.css";
+import { Sidebar } from "./components/sidebar";
 
 export function App() {
   return (
     <div>
-      <Post
-        author="Thiago Razzini"
-        content="
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Thiago Razzini"
+            content="
           Lorem ipsum dolor sit amet 
           consectetur adipisicing elit. Perferendis velit aut temporibus ratione, 
           praesentium quos distinctio nihil eos optio earum omnis suscipit, numquam 
           illum perspiciatis? Porro nihil nesciunt consectetur excepturi!"
-      />
-      <Post author="Bianca Ferreira" content="Um novo post massa!" />
+          />
+          <Post author="Bianca Ferreira" content="Um novo post massa!" />
+        </main>
+      </div>
     </div>
   );
 }
